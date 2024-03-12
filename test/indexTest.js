db0df61f466ed152f8d32d63e155f5c7fb002afa
victor@victor <victor.dmaina@gmail.com>
2024-03-12 14:33:55 +0300

    add

--- test/indexTest.js
+++ test/indexTest.js
@@ -64,10 +64,7 @@ function sayHiToHeadphonedRoommate(string) {
       return 'I would love to!';
     }
   }
-   else {
-    console.error('Error: Input must be a string');
-    return null;
-  }
+   
 }
 
 describe('sayHiToHeadphonedRoommate(string)', function() {
@@ -87,18 +84,3 @@ describe('sayHiToHeadphonedRoommate(string)', function() {
 })
 
 
-describe('sayHiToHeadphonedRoommate(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToHeadphonedRoommate('hello')).toEqual("I can't hear you!");
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToHeadphonedRoommate('HELLO')).toEqual("YES INDEED!");
-  })
-
-  it('returns "I would love to!" if `string` is "Let\'s have dinner together!"`', function () {
-    expect(sayHiToHeadphonedRoommate("Let's have dinner together!")).toEqual(
-      "I would love to!"
-    );
-  });
-})
